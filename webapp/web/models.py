4327be72e1ab4650534acff20adaf46fbeee8069
Zeroitman <borsh130894@gmail.com>
2019-03-21 17:33:00 +0600

    Добавлена модель Task

--- webapp/web/models.py
+++ webapp/web/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    TURN = 'Очередь'
+    IN_WORK = 'В работе'
+    MADE = 'Сделано'
+    VIEW = (
+        (TURN, 'Очередь'),
+        (IN_WORK, 'В работе'),
+        (MADE, 'Сделано'),
+    )
+    summary = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    due_date = models.DateTimeField()
+    status = models.CharField(max_length=30, choices=VIEW, default=TURN, null=False)
+    time_planned = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+
+    def __str__(self):
+        return self.summary
+
