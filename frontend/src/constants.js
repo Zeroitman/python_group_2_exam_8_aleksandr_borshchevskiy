b97757b16d7d59637396fb626516fec2d5229773
Zeroitman <borsh130894@gmail.com>
2019-03-24 15:26:10 +0600

    Добавлено редактирование и удаление, а также dump.json

---
+++ frontend/src/constants.js
@@ -0,0 +1,6 @@
+const TASK_STATUS_QUEUE = {value: 'queue', label: 'Очередь'};
+const TASK_STATUS_IN_PROGRESS = {value: 'in_progress', label: 'В процессе'};
+const TASK_STATUS_DONE = {value: 'done', label: 'Сделано'};
+const TASK_STATUSES = [TASK_STATUS_QUEUE, TASK_STATUS_IN_PROGRESS, TASK_STATUS_DONE];
+
+export {TASK_STATUS_QUEUE, TASK_STATUS_IN_PROGRESS, TASK_STATUS_DONE, TASK_STATUSES};
--- webapp/web/models.py
+++ webapp/web/models.py
@@ -1,21 +1,18 @@
 from django.db import models
 
+VIEW = (
+    ('queue', 'Queue'),
+    ('in_progress', 'In Progress'),
+    ('done', 'Done')
+)
+
 
 class Task(models.Model):
-    TURN = 'Очередь'
-    IN_WORK = 'В работе'
-    MADE = 'Сделано'
-    VIEW = (
-        (TURN, 'Очередь'),
-        (IN_WORK, 'В работе'),
-        (MADE, 'Сделано'),
-    )
     summary = models.CharField(max_length=255)
     description = models.TextField(max_length=2000, null=True, blank=True)
     due_date = models.DateTimeField()
-    status = models.CharField(max_length=30, choices=VIEW, default=TURN, null=False)
+    status = models.CharField(max_length=30, choices=VIEW, default='queue', null=False)
     time_planned = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
 
     def __str__(self):
         return self.summary
-
